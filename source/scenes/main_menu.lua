989c13b7b616b23e5db1c983e45f72633f89cd82
Brett Chalupa <brett@brettchalupa.com>
2026-04-13 19:20:03 -0400

    feat(main-menu): menu with option select

--- source/scenes/main_menu.lua
+++ source/scenes/main_menu.lua
@@ -6,13 +6,39 @@ local VERSION_TEXT_WIDTH = Fonts.asheville14Bold:getTextWidth(version)
 local ASHEVILLE14_HEIGHT = Fonts.asheville14Bold:getHeight()
 local MARGIN = 10
 
+local OPTION = {
+  PLAY = 1,
+  CREDITS = 2,
+}
+local options = {
+  [OPTION.PLAY] = "Play",
+  [OPTION.CREDITS] = "Credits",
+}
+local currentOption = 1
+
 local function update(_dt)
-  if playdate.buttonJustPressed(playdate.kButtonA) then
-    SwitchScene(SCENE.GAMEPLAY)
+  if playdate.buttonJustPressed(playdate.kButtonUp) then
+    currentOption -= 1
+    if currentOption <= 0 then
+      currentOption = #options
+    end
   end
 
-  if playdate.buttonJustPressed(playdate.kButtonB) then
-    SwitchScene(SCENE.CREDITS)
+  if playdate.buttonJustPressed(playdate.kButtonDown) then
+    currentOption += 1
+    if currentOption > #options then
+      currentOption = 1
+    end
+  end
+
+  if playdate.buttonJustPressed(playdate.kButtonA) then
+    -- TODO(sfx)
+
+    if currentOption == OPTION.PLAY then
+      SwitchScene(SCENE.GAMEPLAY)
+    elseif currentOption == OPTION.CREDITS then
+      SwitchScene(SCENE.CREDITS)
+    end
   end
 
   gfx.clear()
@@ -23,6 +49,17 @@ local function update(_dt)
   SetFont(Fonts.default)
   gfx.drawText(md.author, 10, DISPLAY_HEIGHT - ASHEVILLE14_HEIGHT - MARGIN)
   gfx.drawText(version, DISPLAY_WIDTH - VERSION_TEXT_WIDTH - MARGIN, DISPLAY_HEIGHT - ASHEVILLE14_HEIGHT - MARGIN)
+
+  local menuStartY = 80
+  local menuYSpacing = 32
+  local menuDotSize = 6
+  for index, value in pairs(options) do
+    gfx.drawText(value, MARGIN + 10, menuStartY + (menuYSpacing * index))
+
+    if currentOption == index then
+      gfx.fillRect(MARGIN, menuStartY + 5 + menuYSpacing * index, menuDotSize, menuDotSize)
+    end
+  end
 end
 
 local scene = {

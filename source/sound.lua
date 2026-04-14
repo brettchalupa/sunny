11eaf40204906fc4188596edbddb3fb87b04f7bf
Brett Chalupa <brett@brettchalupa.com>
2026-04-13 21:04:53 -0400

    tweak: lower sfx vol

--- source/sound.lua
+++ source/sound.lua
@@ -2,5 +2,5 @@ local synth = playdate.sound.synth.new(playdate.sound.kWaveSine)
 
 -- Plays the specified note in the kWaveSine synth
 function PlaySFX(note)
-  synth:playMIDINote(note, 0.8, 0.10)
+  synth:playMIDINote(note, 0.5, 0.10)
 end
